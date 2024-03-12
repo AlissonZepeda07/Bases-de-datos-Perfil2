f856a73a3976a5d770ebe93a2c35408bc3ff7ae6
Ricardo Nicolas Melara <ricardo06melara@gmail.com>
2024-03-12 10:53:12 -0600

    actualizacion

--- codigo completo V.2.sql
+++ codigo completo V.2.sql
@@ -62,13 +62,6 @@ CREATE TABLE tb_categorias_productos(
 	imagen_categoria_producto			VARCHAR(50)
 );
 
-CREATE TABLE tb_subcategoria(
-	id_subcategoria			INT AUTO_INCREMENT PRIMARY KEY ,
-	nombre_subcategoria		VARCHAR(50) not null unique,
-	descripcion_subcategoria	VARCHAR(50) not null,
-	id_categoria				INT,
-	FOREIGN KEY (id_categoria) REFERENCES tb_categorias_productos (id_categoria_producto)
-);
 
 
 
@@ -83,8 +76,8 @@ CREATE TABLE tb_productos(
     nombre_producto VARCHAR(50) not null,
     descripcion_producto VARCHAR(500) not null,
     precio_producto float not null,
-    id_subcat INT,
-    FOREIGN KEY (id_subcat) REFERENCES tb_subcategoria(id_subcategoria),
+    id_categoria_producto INT,
+    FOREIGN KEY (id_categoria_producto) REFERENCES tb_categorias_productos(id_categoria_producto),
     id_empleado INT,
     FOREIGN KEY (id_empleado) REFERENCES tb_empleados(id_empleado),
     id_marca	INT,
@@ -126,3 +119,69 @@ FOREIGN KEY (id_producto) REFERENCES tb_productos (id_producto),
 precio_pedido	float,
 cantidad_pedido	INT
 );
+
+
+## Creacion de procedimiento almacenado para la tabla categorias
+
+DELIMITER //
+
+CREATE PROCEDURE InsertarCategoriaProducto(
+    IN nombre_categoria_producto_param VARCHAR(50),
+    IN descripcion_categoria_producto_param VARCHAR(50),
+    IN imagen_categoria_producto_param VARCHAR(50)
+)
+BEGIN
+    INSERT INTO tb_categorias_productos (
+        nombre_categoria_producto,
+        descripcion_categoria_producto,
+        imagen_categoria_producto
+    ) VALUES (
+        nombre_categoria_producto_param,
+        descripcion_categoria_producto_param,
+        imagen_categoria_producto_param
+    );
+END //
+
+DELIMITER ;
+
+CALL InsertarCategoriaProducto('Electrónicos', 'Productos electrónicos', 'imagen_electronicos.jpg');
+
+SELECT * FROM tb_categorias_productos;
+
+
+## Creacion de funcion para la tabla comentarios, esta funcion lo que estara realizando sera contar los comentarios que un producto tenga
+
+DELIMITER //
+
+CREATE FUNCTION ComentariosPorProducto (id_producto INT) 
+RETURNS INT
+BEGIN
+    DECLARE total_comentarios INT;
+
+    SELECT COUNT(*) INTO total_comentarios
+    FROM tb_comentarios
+    WHERE id_producto = id_producto;
+
+    RETURN total_comentarios;
+END //
+
+DELIMITER ;
+
+SELECT ComentariosPorProducto(1); 
+
+
+## Creacion de Triggers. aca lo que hara este trigger es cambiar el estado del cliente 
+
+DELIMITER //
+
+CREATE TRIGGER actualizar_estado_empleado
+BEFORE UPDATE ON tb_empleados
+FOR EACH ROW
+BEGIN
+    IF NEW.estado_cliente NOT IN ('conectado', 'desconectado', 'inutilizable', 'baneado') THEN
+        SET NEW.estado_cliente = 'inutilizable';
+    END IF;
+END //
+
+DELIMITER ;
+
